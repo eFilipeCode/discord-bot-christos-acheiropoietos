0e25c58189a1f699af4471c1735ac2b4707a6108
Emanuel Filipe <efilipecode@gmail.com>
2021-03-03 18:05:24 -0300

    Updates

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const Discord = require("discord.js");
 const MessageController = require('../src/controllers/MessageController.js');
 require('dotenv').config();
 
 const app = express();
+app.listen(process.env.PORT || 3333);
+app.use(cors());
 
 const client = new Discord.Client();
 
@@ -11,5 +14,3 @@ const prefix = "!xp "
 
 client.on("message", (message) => MessageController(message, prefix));
 client.login(process.env.BOT_TOKEN);
-
-app.listen(process.env.PORT || 3333);
